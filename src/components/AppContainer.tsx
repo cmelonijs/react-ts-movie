35a984641cfaa735c94854fad10c6e31f237ec4c
Carlo Meloni <carlo.meloni@ped.technology>
2023-01-03 10:10:18 +0100

    creating an app container component

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AppContainer = () => {
+    return (
+        <>
+            <React.StrictMode>
+                <h3>header</h3>
+                <h1>app</h1>
+                <h3>footer</h3>
+            </React.StrictMode>
+        </>
+    )
+}
+
+export default AppContainer;    
