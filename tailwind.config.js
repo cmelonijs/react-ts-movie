cdfd150b3393596cec1338e2ff801f3e1bdfdbd0
Carlo Meloni <carlo.meloni@ped.technology>
2023-01-03 09:57:32 +0100

    adding tailwind and color variables

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['.src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#423F71',
+        header: '#292841',
+        body: '#1C1829'
+      }
+    },
+  },
+  plugins: ['@tailwindcss/line-clamp'],
+}
