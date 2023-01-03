b7410022560334e0d86b92c8b581338fa78d322f
Carlo Meloni <carlo.meloni@ped.technology>
2023-01-03 11:52:36 +0100

    minor fix in tailwind config and styles for the header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,51 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Container from "../components/Container";
+import { mergeClassName } from "../utils";
+
+const MENU_CLASS = `
+    px-1.5
+    py-1.5 
+    hover:bg-primary
+    rounded-md
+`;
+
+const MENU_CLASS_ACTIVE = `
+    bg-primary
+`;
+
+
 const Header = () => {
+    const location = useLocation();
+    const [pathname, setPathname] = useState<string>('');
+
+    useEffect(() => {
+        setPathname(location.pathname)
+    }, [location.pathname]);
+
+    const getMenuClass = (path: string) => {
+        return path === pathname ? mergeClassName(MENU_CLASS, MENU_CLASS_ACTIVE) : mergeClassName(MENU_CLASS)
+    }
+
     return (
-        <h1>HEADER</h1>
+        <div className="bg-header">
+            <Container className="flex justify-between">
+                <div className="flex items-center gap-6">
+                    {/* brand */}
+                    <h1 className="text-2xl font-semibold">
+                        <Link to="/">
+                            Netflix
+                        </Link>
+                    </h1>
+                    {/* menu */}
+                    <div className="flex items-center gap-1.5">
+                        <Link className={getMenuClass('/movies')} to="/movies">Movies</Link>
+                        <Link className={getMenuClass('/tv')} to="/tv">TV</Link>
+                    </div>
+                </div>
+                {/* search */}
+            </Container>
+        </div>
     )
 }
 
